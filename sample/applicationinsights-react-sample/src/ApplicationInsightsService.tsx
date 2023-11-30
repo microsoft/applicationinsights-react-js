31dfb59203b990c4aa86def2414dfdc8f3146ef9
ravindracs0071 <ravindracs0071@gmail.com>
2023-11-30 20:18:54 +0530

    Update ApplicationInsightsService.tsx
    add custom property support

--- sample/applicationinsights-react-sample/src/ApplicationInsightsService.tsx
+++ sample/applicationinsights-react-sample/src/ApplicationInsightsService.tsx
@@ -19,6 +19,12 @@ appInsights.loadAppInsights();
 appInsights.addTelemetryInitializer((env:ITelemetryItem) => {
     env.tags = env.tags || [];
     env.tags["ai.cloud.role"] = "testTag";
+    //custom props
+    if (env.data) {
+            env.data["ms-appName"] = "<app-name>";
+            env.data["ms-user"] = "user-info";
+            env.data["ms-userid"] = "auth-id";
+        }
 });
 
 export { reactPlugin, appInsights };
